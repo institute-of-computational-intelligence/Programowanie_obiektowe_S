406cf5614af0e0039c0787ee79436b1d972a894e
Tomasz B <tom.bil@interia.pl>
2020-04-03 14:33:44 +0200

    lab2

---
+++ lab2/lab2/HandballPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class HandballPlayer : Player
+    {
+        public HandballPlayer() : base() { }
+        public HandballPlayer(string firstName, string lastName, DateTime birthDate, string position, string club) : base(firstName, lastName, birthDate, position, club) { }
+        public override void ScoreGoal()
+        {
+            base.ScoreGoal();
+            Console.WriteLine("Handball player scored a goal!");
+        }
+    }
+}
