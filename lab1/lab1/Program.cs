dcc793fceaa1d90d61b76e4e3946ae25ed8ca707
Bolesław B <bolekbiela7@gmail.com>
2020-04-19 14:38:46 +0200

    lab1

--- lab1/lab1/Program.cs
+++ lab1/lab1/Program.cs
@@ -6,18 +6,79 @@ using System.Threading.Tasks;
 
 namespace lab1
 {
-    public class car
+    public class Car
     {
-        private string brand;
-        private string model;
-        private int doors;
-        private int engine;
-        private double fuel_cons;
-    }
-    public Brand
+        private string _brand;
+        private string _model;
+        private int _doors;
+        private float _engine;
+        private double _fuelCons;
+
+        private static int _carCount = 0;
+        
+        public string Brand
         {
-        return brand
-}
+            get { return _brand; }
+            set { _brand = value; }
+        }
+        public string Model
+        {
+            get { return _model; }
+            set { _model = value; }
+        }
+        public int Doors
+        {
+            get { return _doors; }
+            set { _doors = value; }
+        }
+        public float Engine
+        {
+            get { return _engine; }
+            set { _engine = value; }
+        }
+        public double FuelCons
+        {
+             get { return _fuelCons; }
+            set { _fuelCons = value; }
+        }
+        public Car()
+        {
+            _brand = "";
+            _model = "";
+            _doors = 0;
+            _engine = 0.0;
+            _fuelCons = 0.0;
+        }
+        public Car(string brand, string model, int doors, float engine, double fuelCons)
+        {
+            _brand = brand;
+            _model = model;
+            _doors = doors;
+            _engine = engine;
+            _fuelCons = fuelCons;
+            _carCount++;
+        }
+        public double CalcCons(double roadLength)
+        {
+            return (_fuelCons * roadLength)/100;
+        }
+        public double CalcCost(double roadLength, double petrolCost)
+        {
+            return CalcCons(roadLength) * petrolCost;
+        }
+        public static void Details()
+        {
+            Console.WriteLine(this);
+        }
+        public static void CarCount()
+        {
+            Console.WriteLine($"Car count: {_carCount}");
+        }
+
+        }
+    }
+    
+
 
 
     class Program
@@ -26,4 +87,4 @@ namespace lab1
         {
         }
     }
-}
+
