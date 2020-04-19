5da67d2ec5ff1e1f05740621dc6cf3adecdf283a
Bolesław B <bolekbiela7@gmail.com>
2020-04-19 13:49:43 +0200

    Lab1

---
+++ Lab1_biela/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_biela
+{
+    class Samochod
+    {
+        string marka;
+        string model;
+        int ilosc_drzwi;
+        int pojemnosc_silnika;
+        int srednie_spalanie; 
+    }
+}
