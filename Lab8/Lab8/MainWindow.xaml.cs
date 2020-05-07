e96a7e986e38e74db2eafaad6bc44212eb7358f2
Jakub Spałek <spalek9822@gmail.com>
2020-05-07 16:20:02 +0200

    Lab 8 task 1 solution

---
+++ Lab8/Lab8/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab8
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Student> Students { get; set; }
+
+        public MainWindow()
+        {
+            Students = new List<Student>
+            {
+                new Student("Jan", "Kowalski", 1033,  "WIMiI"),
+                new Student("Michał", "Nowak", 1013,  "WIMiI"),
+                new Student("Jacek", "Makieta", 1021,  "WIMiI")
+            };
+
+            InitializeComponent();
+
+            StudentDataGrid.Columns.Add(new DataGridTextColumn() { Header = "Imię", Binding = new Binding("Name") });
+            StudentDataGrid.Columns.Add(new DataGridTextColumn() { Header = "Nazwisko", Binding = new Binding("Surname") });
+            StudentDataGrid.Columns.Add(new DataGridTextColumn() { Header = "Nr Indeksu", Binding = new Binding("IndexNo") });
+            StudentDataGrid.Columns.Add(new DataGridTextColumn() { Header = "Wydział", Binding = new Binding("Faculty") });
+
+            StudentDataGrid.AutoGenerateColumns = false;
+            StudentDataGrid.ItemsSource = Students;
+        }
+
+        private void AddStudentButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new StudentWindow();
+
+            if(dialog.ShowDialog() == true)
+            {
+                Students.Add(dialog.Student);
+                StudentDataGrid.Items.Refresh();
+            }
+        }
+
+        private void RemoveStudentButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(StudentDataGrid.SelectedItem is Student)
+            {
+                Students.Remove((Student)StudentDataGrid.SelectedItem);
+                StudentDataGrid.Items.Refresh();
+            }
+        }
+    }
+}
---
+++ Lab8/Lab8/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lab8
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Faculty { get; set; }
+        public int IndexNo { get; set; }
+
+        public Student()
+        { }
+
+        public Student(string name, string surname, int indexNo, string faculty)
+        {
+            Name = name;
+            Surname = surname;
+            IndexNo = indexNo;
+            Faculty = faculty;
+        }
+    }
+}
