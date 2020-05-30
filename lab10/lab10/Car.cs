e57766dbd8a4a1796d6f6bfddd0682858675d6d4
aleksandrap <aleksandra.pyrkosz@gmail.com>
2020-05-30 17:55:05 +0200

    lab10 niecale

---
+++ lab10/lab10/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    [DB]
+    public class Car
+    {
+        [DBC(Title = "Rejestracja")]
+        public string Registration_Number { get; set; }
+        [DBC(Title = "Kolor")]
+        public string Color { get; set; }
+        [DBC(Title = "Marka")]
+        public string Brand { get; set; }
+        public Person Owner { get; set; }
+    }
+}
---
+++ lab10/lab10/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    [DB]
+    public class Person
+    {
+        [DBC(Title = "Imie")]
+        public string Name { get; set; }
+        [DBC(Title = "Nazwisko")]
+        public string Surname { get; set; }
+    }
+}
